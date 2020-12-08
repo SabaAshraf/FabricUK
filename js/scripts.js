0841a367b6a6f8b8b4371e3d8dce1b91b42b4ec6
Saba Ashraf <saba.ashraf75@gmail.com>
2020-12-08 16:27:22 +0530

    adding files to the repository

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.sliderContent').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        
+    });
+  
+});
